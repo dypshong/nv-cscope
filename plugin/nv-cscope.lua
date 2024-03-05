ba2556cb676bac5bae4fc69b9f5ea448e41bf137
Daeyoung Park <dypshong@gmail.com>
2024-03-05 19:22:28 +0900

    ee

--- plugin/nv-cscope.lua
+++ plugin/nv-cscope.lua
@@ -12,6 +12,6 @@ end,
     complete = require('nv-cscope.completion'),
 
 })
-command! -nargs=* -complete=customlist,v:lua.require'nv-cscope.completion' NvCscope lua require("nv-cscope").cscope(<args>)
+--command! -nargs=* -complete=customlist,v:lua.require'nv-cscope.completion' NvCscope lua require("nv-cscope").cscope(<args>)
 
 vim.api.nvim_set_var('loaded_nv_cscope')
